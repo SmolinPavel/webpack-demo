71d349eaf6b707f8c9d40ed5131a1a5c16137941
Pavel Smolin <isingwithaz@gmail.com>
2018-12-26 17:08:35 +0300

    Add image loader and demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
 
+import './style.css';
+import Santa from './santa.png';
+
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const santaImg = new Image();
+  santaImg.src = Santa;
+
+  element.appendChild(santaImg);
 
   return element;
 }
