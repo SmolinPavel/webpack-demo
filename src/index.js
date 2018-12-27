cb2a4b9bec8cc9e29eb166dadc064c49964e8733
Pavel Smolin <isingwithaz@gmail.com>
2018-12-27 17:15:38 +0300

    Cleaning

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash';
 
-import './style.css';
-import Santa from './santa.png';
-import Data from './data.xml';
-
 function component() {
-  let element = document.createElement('div');
+  const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const santaImg = new Image();
-  santaImg.src = Santa;
-
-  element.appendChild(santaImg);
-
-  console.log(Data);
 
   return element;
 }
